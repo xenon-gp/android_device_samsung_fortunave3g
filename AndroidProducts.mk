ead45cfca7202cbe943c107afd18338f918e322c
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-04-27 22:05:07 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,5 @@ LOCAL_PATH := device/samsung/fortunave3g
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_fortunave3g.mk \
 	$(LOCAL_DIR)/pa_fortunave3g.mk \
-	$(LOCAL_DIR)/lineage_fortunave3g.mk
+	$(LOCAL_DIR)/lineage_fortunave3g.mk \
+	$(LOCAL_DIR)/aosp_fortunave3g.mk
