800f2b9c9b494eb695eb496bc5d7e3f00aee5793
Phan Duc <38982972+Gumball1506@users.noreply.github.com>
2019-06-29 21:33:34 +0700

    Update and rename havoc_fortunave3g.mk to xenonhd_fortunave3g.mk

--- xenonhd_fortunave3g.mk
+++ xenonhd_fortunave3g.mk
@@ -1,10 +1,10 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/havoc.mk)
+$(call inherit-product, device/samsung/gprimelte-common/xenonhd.mk)
 $(call inherit-product, device/samsung/fortunave3g/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fortunave3g
-PRODUCT_NAME := havoc_fortunave3g
+PRODUCT_NAME := xenonhd_fortunave3g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
